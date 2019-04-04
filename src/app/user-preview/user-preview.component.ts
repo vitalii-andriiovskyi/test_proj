2ae51269be973868f221e753a4af33e426709258
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 11:31:55 +0300

    feat: UserPreviewComp: prop user, html, css-stylings

--- src/app/user-preview/user-preview.component.ts
+++ src/app/user-preview/user-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'ntw-user-preview',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-preview.component.scss']
 })
 export class UserPreviewComponent implements OnInit {
+  user: User = {
+    id: 1,
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jp'
+  };
 
   constructor() { }
 
