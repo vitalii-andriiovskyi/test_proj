956669e20b134db023c63d30927f7ba767a61ff8
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 12:35:39 +0300

    feat: UserDetailComponent: support for getting user from server

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user.model';
+import { UserService } from '../services/user-service.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ntw-user-detail',
@@ -8,13 +12,23 @@ import { User } from '../../models/user.model';
 })
 export class UserDetailComponent implements OnInit {
 
-  user: User = {
-    id: 1,
-    first_name: 'George',
-    last_name: 'Bluth',
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jp'
-  };
-  constructor() { }
+  // user: User = {
+  //   id: 1,
+  //   first_name: 'George',
+  //   last_name: 'Bluth',
+  //   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jp'
+  // };
+
+  user$: Observable<User>;
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {
+    this.user$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.userService.getUserData(params.get('id')))
+      );
+    }
 
   ngOnInit() {
   }
