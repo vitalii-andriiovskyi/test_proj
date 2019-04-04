7e43fd8246a5314c43a0f1101af417d3b0c361ff
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 14:05:01 +0300

    feat: models: UserListData

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,6 +1,14 @@
 export interface User {
-  id: string;
+  id: number;
   first_name: string;
   last_name: string;
   avatar: string;
+}
+
+export interface UserListData {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: User[];
 }
