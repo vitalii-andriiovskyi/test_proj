31ca3f87d0ee8ce332b5a0b128a2f47d2c85cde7
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 11:35:48 +0300

    refactor(UserPreviewComp): prop user

--- src/app/user-preview/user-preview.component.ts
+++ src/app/user-preview/user-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../models/user.model';
 
 @Component({
@@ -7,12 +7,8 @@ import { User } from '../models/user.model';
   styleUrls: ['./user-preview.component.scss']
 })
 export class UserPreviewComponent implements OnInit {
-  user: User = {
-    id: 1,
-    first_name: 'George',
-    last_name: 'Bluth',
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jp'
-  };
+  @Input() user: User;
+
 
   constructor() { }
 
