9560ab902f3756b22a4c48b070b4089008dd2f6a
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 10:54:14 +0300

    create ntw-project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ntw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ntw-project';
+}
