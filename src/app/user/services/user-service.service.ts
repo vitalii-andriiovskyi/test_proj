aeae7e426f6cfcd8f942dd330436e7ba1fa8398c
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 12:34:43 +0300

    feat: UserService: handleError(), getUserData()

--- src/app/user/services/user-service.service.ts
+++ src/app/user/services/user-service.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, shareReplay } from 'rxjs/operators';
+import { User, UserListData } from 'src/app/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  userUrl = `https://reqres.in/api/users/`;
+  userListUrl = 'https://reqres.in/api/users';
+  
+  userFallback: User = {
+    id: 1,
+    first_name: 'No name',
+    last_name: 'No last name',
+    avatar: ''
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserData(id: string): Observable<User> {
+    return this.http.get<User>(`${this.userUrl}${id}`).pipe(
+      shareReplay(), // to avoid extra calls to the same endpoint
+      catchError(this.handleError(this.userFallback))
+    );
+  }
+
+  handleError = (data: any) => (err: any) => {
+    console.warn(err); // there must be custom logger
+    return of(data);
+  }
 }
