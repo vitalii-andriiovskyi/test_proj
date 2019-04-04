6656cb7741fd6600509e3c936cbc074504a302cb
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 14:05:39 +0300

    refactor: SharedModule: import materialsModules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+const modules = [
+  CommonModule,
+  MatCardModule,
+  MatPaginatorModule
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: modules,
+  exports: modules
 })
 export class SharedModule { }
