4cc94e92f09c1839cade200a53e7d2c787905116
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 11:33:17 +0300

    feat: path to UserDetailComp

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from '../user-list/user-list.component';
+import { UserDetailComponent } from '../user-detail/user-detail.component';
 
 const routes: Routes = [
   { path: '', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailComponent },
 ];
 
 @NgModule({
