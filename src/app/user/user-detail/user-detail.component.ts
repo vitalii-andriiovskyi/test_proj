467aef05722ac5d96fa7377860ca41b5c96f5779
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 14:46:26 +0300

    feat: UserDetailComponent: back button

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { User } from '../../models/user.model';
 import { UserService } from '../services/user-service.service';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap, tap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, ParamMap, Router, NavigationEnd } from '@angular/router';
+import { switchMap, tap, filter, takeUntil } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { RouterHistoryService } from 'src/app/core/services/router-history.service';
 
 @Component({
   selector: 'ntw-user-detail',
@@ -18,19 +19,22 @@ export class UserDetailComponent implements OnInit {
   //   last_name: 'Bluth',
   //   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jp'
   // };
+  previousUrl: string;
 
   user$: Observable<User>;
   constructor(
     private userService: UserService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private routerHistoryService: RouterHistoryService
   ) {
     this.user$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) =>
         this.userService.getUserData(params.get('id')))
       );
-    }
+  }
 
   ngOnInit() {
+    this.previousUrl = this.routerHistoryService.getPreviousUrl();
   }
 
 }
