fc1e011041caab4146f59954a45a9f8739b80214
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-04-04 12:40:49 +0300

    feat: UserListComponent: destroy$ prop

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { User } from '../../models/user.model';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'ntw-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
   user: User = {
     id: 1,
     first_name: 'George',
     last_name: 'Bluth',
     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jp'
   };
+
+  destroy$ = new Subject();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 
 }
